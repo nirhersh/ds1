d28863f6958c4fce35bbb77b104c45efd0546111
Roei Bloch <roei.bloch@campus.technion.ac.il>
2022-11-24 15:28:06 +0200

    remove function
    Co-authored-by: nirhersh <nirhersh@users.noreply.github.com>

--- AVLTree.h
+++ AVLTree.h
@@ -37,6 +37,10 @@ private:
     Node* root;
     void fix_height(Node* node);
     void balance_tree(Node* newNode);
+    Node* search_node(const K& key) const;
+    Node* remove_node(Node* nodeToRemove);
+    Node* get_following_node(Node* node)const;
+    void switch_nodes(Node* node1,Node* node2);
     int calc_BF(Node* node);
     void LL_fix(Node* node);
     void RL_fix(Node* node);
@@ -71,28 +75,12 @@ AVLTree<T, K>::AVLTree(): root(nullptr){}
 
 template <class T, class K>
 T* AVLTree<T, K>::search(const K& key) const{
-    bool found = false;
-    Node* currentNode = root;
-    while (!found)
-    {
-        if(currentNode == nullptr)
-        {
-            throw KeyDoesntExists();
-        }
-        if (key > currentNode->m_key){
-            currentNode = currentNode->m_right;
-        }else if(key == currentNode->m_key){
-            found = true;
-            return currentNode->m_data;
-        }else{
-            currentNode = currentNode->m_left;
-        }
-    }
+    Node* nodeToReturn = search_node(key);
+    return nodeToReturn->m_data;
 }
 
 template <class T, class K>
 void AVLTree<T, K>::push(T* item, K& key){
-    std::cout << "entering: " << *item << std::endl;
     assert(item != nullptr);
     bool found = false;
     Node* currentNode = root;
@@ -146,12 +134,10 @@ void AVLTree<T, K>::balance_tree(Node* newNode)
         }
         tempNode->m_parent->m_height = tempNode->m_height + 1;
         int balance =calc_BF(tempNode->m_parent);
-        std::cout << "balance of "<< *(tempNode->m_parent->m_data) << "is:" << balance << std::endl; 
         if(balance == 2)
         {
             if(calc_BF(tempNode) > -1)
             {
-                std::cout << "entered ll:" << std::endl;
                 LL_fix(tempNode->m_parent);
             } else {
                 LR_fix(tempNode->m_parent);
@@ -161,7 +147,6 @@ void AVLTree<T, K>::balance_tree(Node* newNode)
         else if(balance == -2){
             if(calc_BF(tempNode) < 1) {
                 RR_fix(tempNode->m_parent);
-                std::cout << "entered rr:" << std::endl;
             } else {
                 RL_fix(tempNode->m_parent);
             }
@@ -202,75 +187,14 @@ void AVLTree<T, K>::LL_fix(Node* parent){
 template<class T, class K>
 void AVLTree<T, K>::LR_fix(Node* parent)
 {
-    Node* CParent = parent->m_parent;
-    Node* C = parent;
-    Node* B = parent->m_left;
-    Node* A = parent->m_left->m_right;
-    Node* BL = A->m_left;
-    Node* BR = A->m_right;
-    assert(parent != nullptr || A != nullptr || B!= nullptr || C!= nullptr);
-    C->m_left = BR;
-    C->m_parent = B;
-    B->m_right = C;
-    B->m_left = A;
-    B->m_parent = CParent;
-    if(BL != nullptr){
-        BL->m_parent = A;
-    }
-    if(BR != nullptr){
-        BR->m_parent = C;
-    }
-    
-    A->m_parent = B;
-    A->m_right = BL;
-    if(CParent)
-    {
-        if(CParent->m_left == C){
-            CParent->m_left = B;
-        }else{
-            CParent->m_right = B;
-        }
-    }
-    if(C == nullptr){
-        root = B;
-    }
-    fix_height(C);
+    RR_fix(parent->m_left);
+    LL_fix(parent);
 }
 
 template<class T, class K>
 void AVLTree<T, K>::RL_fix(Node* parent){
-    Node* C = parent;
-    Node* CParent = C->m_parent;
-    Node* A = parent->m_right;
-    Node* B = A->m_left;
-    Node* BL = B->m_left;
-    Node* BR = B->m_right;
-    assert(parent != nullptr || A != nullptr || B!= nullptr || C!= nullptr);
-    C->m_parent = B;
-    C->m_right = BL;
-    B->m_parent = CParent;
-    B->m_left = C;
-    if(BR != nullptr){
-    BR->m_parent = A;
-    }
-    if(BL != nullptr){
-    BL->m_parent = C;
-    }
-    B->m_right = A;
-    A->m_left = BR;
-    A->m_parent = B;
-    if(C == root){
-        root = B;
-    }
-    if(CParent)
-    {
-        if(CParent->m_left == C){
-            CParent->m_left = B;
-        }else{
-            CParent->m_right = B;
-        }
-    }
-    fix_height(C);
+    LL_fix(parent->m_right);
+    RR_fix(parent);
 }
 
 template<class T, class K>
@@ -299,7 +223,6 @@ void AVLTree<T, K>::RR_fix(Node* parent)
             A->m_parent->m_right = A;
         }
     }
-    std::cout<< *(B->m_parent->m_data) <<"Dfs"<< std::endl;
     fix_height(B);
 }
 
@@ -323,8 +246,117 @@ void AVLTree<T, K>::fix_height(Node* node)
     node->m_height = leftTreeH > rightTreeH ? 1 + leftTreeH : 1 + rightTreeH;
 }
 
+template<class T, class K>
+void AVLTree<T, K>::remove(const K& key)
+{
+    Node* nodeToRemove = search_node(key);
+    Node* currentNode = remove_node(nodeToRemove); 
+    Node* parent = currentNode->m_parent;
+    do{
+        int parentHeight = currentNode->m_parent? currentNode->m_parent->m_height: 0;
+        fix_height(currentNode);
+        balance_tree(currentNode);
+        if(parent == nullptr || parentHeight == parent->m_height){
+            break;
+        }
+        currentNode = currentNode->m_parent;
+        parent = currentNode->m_parent;
+    } while(currentNode != root);
 
 
+
+}
+
+template<class T, class K>
+typename AVLTree<T, K>::Node* AVLTree<T, K>::search_node(const K& key) const
+{
+    bool found = false;
+    Node* currentNode = root;
+    while (!found)
+    {
+        if(currentNode == nullptr)
+        {
+            throw KeyDoesntExists();
+        }
+        if (key > currentNode->m_key){
+            currentNode = currentNode->m_right;
+        }else if(key == currentNode->m_key){
+            found = true;
+            return currentNode;
+        }else{
+            currentNode = currentNode->m_left;
+        }
+    }
+}
+
+template<class T, class K>
+typename AVLTree<T, K>::Node* AVLTree<T, K>::remove_node(Node* nodeToRemove){
+    bool isRight = false;
+    if(nodeToRemove->m_parent->m_right == nodeToRemove){
+            isRight = true;;
+    }
+    Node* nodeToReturn;
+    if (nodeToRemove->m_height == 0)
+    {
+        if(!isRight){
+           nodeToRemove->m_parent->m_left = nullptr; 
+        }
+        else{
+            nodeToRemove->m_parent->m_right = nullptr;
+        }
+        nodeToReturn = nodeToRemove->m_parent;
+        delete nodeToRemove; 
+    }
+    if(nodeToRemove->m_right != nullptr && nodeToRemove->m_left == nullptr){
+        if(isRight){
+            nodeToRemove->m_parent->m_right = nodeToRemove->m_right;
+        }
+        else {
+            nodeToRemove->m_parent->m_left = nodeToRemove->m_right;
+        }
+        nodeToRemove->m_right->m_parent = nodeToRemove->m_parent;
+        nodeToReturn = nodeToRemove->m_right;
+        delete nodeToRemove;
+    }
+    else if(nodeToRemove->m_right == nullptr && nodeToRemove->m_left != nullptr){
+        if(isRight){
+            nodeToRemove->m_parent->m_right = nodeToRemove->m_left;
+        }
+        else {
+            nodeToRemove->m_parent->m_left = nodeToRemove->m_left;
+        }
+        nodeToRemove->m_left->m_parent = nodeToRemove->m_parent;
+        nodeToReturn = nodeToRemove->m_left;
+        delete nodeToRemove;
+    }
+    else{
+        Node* followingNode = get_following_node(nodeToRemove);
+        switch_nodes(NodeToRemove, followingNode);
+        remove_node(nodeToRemove);    
+    }
+    return nodeToReturn;
+}
+
+template<class T, class K>
+void AVLTree<T, K>::switch_nodes(Node* node1, Node* node2){
+    
+}
+
+
+
+
+template<class T, class K> 
+typename AVLTree<T, K>::Node* AVLTree<T, K>::get_following_node(Node* node) const
+{
+    assert(node != nullptr);
+    assert(node->m_right != nullptr);
+    node = node->m_right;
+    while(node->m_left)
+    {
+        node = node->m_left;
+    }
+    return node;
+}
 
 #endif
 
--- testAVL.cpp
+++ testAVL.cpp
@@ -7,12 +7,13 @@
 int main(){
     //std::cout << "fwfdew";
     AVLTree<int, int> avlTree1;
-    int x = 5;
-    int y = 6;
-    int z = 7;
-    int w = 8;
-    int a = 4;
-    int b = 3;
+    int x = 10;
+    int y = 12;
+    int z = 11;
+    int w = 7;
+    int a = 8;
+    int b = 5;
+    int c = 4;
 
 
     avlTree1.push(&x, x);
@@ -21,6 +22,8 @@ int main(){
     avlTree1.push(&w, w);
     avlTree1.push(&a, a);
     avlTree1.push(&b, b);
+    avlTree1.push(&c, c);
+    avlTree1.remove(4);
 
     std::cout<< *(avlTree1.getRoot()->m_data) << std::endl;
     std::cout<< *(avlTree1.getRoot()->m_left->m_data) << std::endl;
@@ -28,7 +31,9 @@ int main(){
 
     std::cout<< *(avlTree1.getRoot()->m_left->m_left->m_data)<< std::endl;
     std::cout<< *(avlTree1.getRoot()->m_left->m_right->m_data) << std::endl;
+    std::cout<< *(avlTree1.getRoot()->m_right->m_left->m_data)<< std::endl;
     std::cout<< *(avlTree1.getRoot()->m_right->m_right->m_data) << std::endl;
+    // std::cout<< *(avlTree1.getRoot()->m_right->m_right->m_data) << std::endl;
     }
 
 
